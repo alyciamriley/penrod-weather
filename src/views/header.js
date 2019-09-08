89503972d8b0332a22da46e080e6f5e3147b0636
alyciamriley <alyciarileym@gmail.com>
2019-09-08 10:09:58 -0500

    heroku troubleshooting

---
+++ src/views/header.js
@@ -0,0 +1,13 @@
+// var m = require("mithril")
+
+// module.exports = {
+//     view: function() {
+//         return m("form", "[
+//             m("label.label", "First name"),
+//             m("input.input[type=text][placeholder=First name]"),
+//             m("label.label", "Last name"),
+//             m("input.input[placeholder=Last name]"),
+//             m("button.button[type=button]", "Save"),
+//         ]")
+//     }
+// }
