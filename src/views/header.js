ef85be9f3199cb6e8aaa8ea6a6e79b8ab1d112c3
alyciamriley <alyciarileym@gmail.com>
2019-09-08 19:32:50 -0500

    tesT

--- src/views/header.js
+++ src/views/header.js
@@ -2,7 +2,7 @@ var m = require("mithril")
 
 var Header = { 
     view: function() {
-       return m(".slds-grid", m("img", {
+       return m(".slds-grid logo", m("img", {
             width: "100%",
             src: "src/views/solid-logo.png",
         }))
